659aa65093903e32265027fd4bf77d34ad67f52c
Bhabani Prasad Dash <bhabani.dash@outlook.com>
2020-06-01 04:16:00 +0530

    nginx fixes

--- client/server/index.js
+++ client/server/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 
-const PORT = 8080;
+const PORT = 3000;
 const app = express();
 
 const router = express.Router();
