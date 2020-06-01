257f3caa690f1a36ef4dbbb8fdab7cfff2b0c644
Bhabani Prasad Dash <bhabani.dash@outlook.com>
2020-06-01 12:36:27 +0530

    aws fix

---
+++ client/build/precache-manifest.023fb645ca3102c85320c99218094857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e31eb5b274700b2423ad4dceac03894",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bcffbe025d711d4da23",
+    "url": "/static/css/main.682140b4.chunk.css"
+  },
+  {
+    "revision": "fffa20c7ac5f870259a2",
+    "url": "/static/js/2.5cc9b16a.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/static/js/2.5cc9b16a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bcffbe025d711d4da23",
+    "url": "/static/js/main.986896e3.chunk.js"
+  },
+  {
+    "revision": "877faaad665d4d4df92a",
+    "url": "/static/js/runtime-main.c8c495f1.js"
+  }
+]);
